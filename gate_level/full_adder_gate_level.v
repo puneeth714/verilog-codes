b796a51efb6761fecf421eec932aebb2af83964a
Puneeth <kpuneeth714@gmail.com>
2022-02-21 21:09:46 +0530

    Adder some more files

--- gate_level/full_adder_gate_level.v
+++ gate_level/full_adder_gate_level.v
@@ -8,25 +8,3 @@ and(w3,a,b);
 xor(s,w1,cin);
 or(c,w2,w3);
 endmodule
-
-module test_fd1;
-reg a,b,cin;
-wire s,cout;
-full_adder f1(a,b,cin,s,cout);
-
-initial begin
-$dumpfile("wave.vcd");
-        $dumpvars(0, test_fd1);
-    a=0;b=0;cin=0;#100;
-    a=0;b=0;cin=1;#100;
-     a=0;b=1;cin=0;#100;
-     a=0;b=1;cin=1;#100;
-     a=1;b=0;cin=0;#100;
-     a=1;b=0;cin=1;#100;
-     a=1;b=1;cin=0;#100;
-     a=1;b=0;cin=1;#100;
-      a=1;b=1;cin=1;#100;
-
-
-end
-endmodule
